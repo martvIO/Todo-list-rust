c9a6b49951ff4bcffe3fcd25006058c5fd260583
martvIO <136852336+martvIO@users.noreply.github.com>
2025-01-18 16:57:06 +0200

    created a simple stucture

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::env;
+struct Todo {
+    todo: Vec<String>,
+    todo_path: String
+}
+
+impl Todo {
+    
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
 }
